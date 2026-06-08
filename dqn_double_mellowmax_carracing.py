12e5d5cd728f2c2b9c432966d29a72c7bad58b84
JCholewinski <jcholewinski9@gmail.com>
2026-06-08 09:32:28 +0900

    fix car race

--- dqn_double_mellowmax_carracing.py
+++ dqn_double_mellowmax_carracing.py
@@ -781,7 +781,7 @@ def select_best_model_seed(results: pd.DataFrame, agent_name: str):
 def main():
     # For quick run: 80-150 episodes.
     # For better results: 300+ episodes.
-    episodes = 120
+    episodes = 80
 
     # Quick version:
     seeds = [0]
